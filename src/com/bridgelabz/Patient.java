4015ac488649c77fef03bc9d13e8b6d5e5824a97
Prince <princek2530@gmail.com>
2022-08-16 10:43:00 +0530

    Multiple Hospital

---
+++ src/com/bridgelabz/Patient.java
@@ -0,0 +1,42 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+class Patient {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Patient Name");
+        String name = sc.next();
+        System.out.println("Enter Patient Age");
+        int age = sc.nextInt();
+        System.out.println("Enter Phone Number");
+        int phone = sc.nextInt();
+        System.out.println("Enter City");
+        String city = sc.next();
+        System.out.println("Enter State");
+        String state = sc.next();
+        System.out.println("Enter Department");
+        String department = sc.next();
+    }
+    public enum department{
+        ONCOLOGY, NEUROLOGY, CARDIOLOGY, GYNOCOLOGY
+    }
+    public void addingMultiplePatient() {
+        System.out.println("Enter Details");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println("Enter Your Name");
+            String name = sc.next();
+            System.out.println("Enter Your Age");
+            int age = sc.nextInt();
+            System.out.println("Enter Phone Number");
+            int phone = sc.nextInt();
+            System.out.println("Enter City");
+            String city = sc.next();
+            System.out.println("Enter State");
+            String state = sc.next();
+            System.out.println("Enter Department");
+            String department = sc.next();
+        }
+    }
+}
